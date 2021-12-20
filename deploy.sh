396a903ab8a5b4b37b007194057840e835629300
korn <kornvik.t@hotmail.com>
2021-12-20 16:19:09 +0900

    update script file

--- deploy.sh
+++ deploy.sh
@@ -13,12 +13,16 @@ ls
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 echo "# kornsinanju.github.io" >> README.md
+
+# for first push
 git init
 git add *
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:kornsinanju/kornsinanju.github.io.git main
+git branch -M main
+git remote add origin https://github.com/kornsinanju/kornsinanju.github.io.git
+git push -u origin main
+# if you are deploying to https://<USERNAME>.github.io (second time and so-on)
+# git push -f git@github.com:kornsinanju/kornsinanju.github.io.git main
 # git pull
 # git push https://github.com/kornsinanju/kornsinanju.github.io main -f
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
